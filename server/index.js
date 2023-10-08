7ee260fbd76df488ffc99f4ed936717b7af20f1e
Shivam <shivamkasoudhan7@gmail.com>
2023-10-09 01:16:33 +0530

    socket and room page

--- server/index.js
+++ server/index.js
@@ -2,11 +2,18 @@ const { Server} = require("socket.io");
 
 const io =new Server(8000,{
   cors:true,
-})
+});
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
+
 
 io.on("connection", (socket) => {
   console.log(`Socket connected` , socket.id);
   socket.on ("room:join",(data)=>{
-    console.log(data);
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketidToEmailMap.set(socket.id, email);
+    io.to(socket.id).emit("room:join",data);
   })
+
 });
